66dd0cf6f920200c6386dc5953b4a2a800cc5cbd
Deepak Katariya <deepak.sorthiya@gmail.com>
2026-06-28 23:39:35 +0530

    add devcontainer config for github codespace and fix imports

--- src/main/java/com/example/ElasticsearchRealWorldApplication.java
+++ src/main/java/com/example/ElasticsearchRealWorldApplication.java
@@ -1,17 +1,9 @@
 package com.example;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.transport.endpoints.BooleanResponse;
 import com.example.config.NativeHints;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
-
-import java.io.IOException;
-
-import static com.example.constant.Constants.*;
 
 @SpringBootApplication
 @ImportRuntimeHints(NativeHints.class)
